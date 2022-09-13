5fe1cc10ec248f934181b5b7561f3633abde5adc
scyjm5 <scyjm5@nottingham.ac.uk>
2022-09-13 15:13:15 +0700

    add car and movement

--- controls.js
+++ controls.js
@@ -4,9 +4,7 @@ class Controls{
         this.left = false;
         this.right = false;
         this.reverse = false;
-
-        this.hello = false;
-
+          
         this.#addKeyboardListeners();
     }
     #addKeyboardListeners(){
